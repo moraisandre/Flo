da4ed8e84ff469b5f31f5ad3859c6439328ec4f9
moraisandre <andrewinvista@gmail.com>
2016-07-03 22:43:34 -0300

    Initial Commit

---
+++ Flo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flo
+//
+//  Created by Andre Morais on 7/3/16.
+//  Copyright © 2016 Andre Morais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
