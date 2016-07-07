902dcccb5082c9ec15d8a220a223cd926401332d
moraisandre <andrewinvista@gmail.com>
2016-07-06 22:47:53 -0300

    Added CounterView

---
+++ Flo/CounterView.swift
@@ -0,0 +1,48 @@
+//
+//  CounterView.swift
+//  Flo
+//
+//  Created by Andre Morais on 7/6/16.
+//  Copyright © 2016 Andre Morais. All rights reserved.
+//
+
+import UIKit
+
+let NoOfGlasses = 8
+let π:CGFloat = CGFloat(M_PI)
+
+@IBDesignable class CounterView: UIView {
+    
+    @IBInspectable var counter: Int = 5
+    @IBInspectable var outlineColor: UIColor = UIColor.blueColor()
+    @IBInspectable var counterColor: UIColor = UIColor.orangeColor()
+    
+    override func drawRect(rect: CGRect) {
+        
+        // 1
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        
+        // 2
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        // 3
+        let arcWidth: CGFloat = 76
+        
+        // 4
+        let startAngle: CGFloat = 3 * π / 4
+        let endAngle: CGFloat = π / 4
+        
+        // 5
+        let path = UIBezierPath(arcCenter: center,
+                                radius: radius/2 - arcWidth/2,
+                                startAngle: startAngle,
+                                endAngle: endAngle,
+                                clockwise: true)
+        
+        // 6
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
+        
+    }
+}
--- Flo/PushButtonView.swift
+++ Flo/PushButtonView.swift
@@ -11,10 +11,13 @@ import UIKit
 @IBDesignable
 
 class PushButtonView: UIButton {
+    
+    @IBInspectable var fillColor: UIColor = UIColor.greenColor()
+    @IBInspectable var isAddButton: Bool = true
 
     override func drawRect(rect: CGRect) {
         let path = UIBezierPath(ovalInRect: rect)
-        UIColor.blueColor().setFill()
+        fillColor.setFill()
         path.fill()
         
         //set up the width and height variables
@@ -31,19 +34,36 @@ class PushButtonView: UIButton {
         //move the initial point of the path
         //to the start of the horizontal stroke
         plusPath.moveToPoint(CGPoint(
-            x:bounds.width/2 - plusWidth/2,
-            y:bounds.height/2))
+            x:bounds.width/2 - plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
         
         //add a point to the path at the end of the stroke
         plusPath.addLineToPoint(CGPoint(
-            x:bounds.width/2 + plusWidth/2,
-            y:bounds.height/2))
+            x:bounds.width/2 + plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
         
+        //Vertical Line
+        
+        if isAddButton {
+            //vertical line code moveToPoint(_:) and addLineToPoint(_:)
+            //move to the start of the vertical stroke
+            plusPath.moveToPoint(CGPoint(
+                x:bounds.width/2 + 0.5,
+                y:bounds.height/2 - plusWidth/2 + 0.5))
+            
+            //add the end point to the vertical stroke
+            plusPath.addLineToPoint(CGPoint(
+                x:bounds.width/2 + 0.5,
+                y:bounds.height/2 + plusWidth/2 + 0.5))
+        }
+        //existing code
         //set the stroke color
         UIColor.whiteColor().setStroke()
         
         //draw the stroke
         plusPath.stroke()
+        
+
     }
 
 }
