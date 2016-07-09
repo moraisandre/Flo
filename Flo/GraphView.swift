3386d1eb052da754e4a6d4f6049df0f5dfbf545b
moraisandre <andrewinvista@gmail.com>
2016-07-08 22:38:49 -0300

    added Animated Transition

---
+++ Flo/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Flo
+//
+//  Created by Andre Morais on 7/7/16.
+//  Copyright © 2016 Andre Morais. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     //Counter outlets
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    var isGraphViewShowing = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +40,29 @@ class ViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+    }
+    
+    @IBAction func counterViewTap(gesture:UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            
+            //hide Graph
+            UIView.transitionFromView(graphView,
+                                      toView: counterView,
+                                      duration: 1.0,
+                                      options: UIViewAnimationOptions.TransitionFlipFromLeft
+                                        | UIViewAnimationOptions.ShowHideTransitionViews,
+                                      completion:nil)
+        } else {
+            
+            //show Graph
+            UIView.transitionFromView(counterView,
+                                      toView: graphView,
+                                      duration: 1.0,
+                                      options: UIViewAnimationOptions.TransitionFlipFromRight
+                                        | UIViewAnimationOptions.ShowHideTransitionViews,
+                                      completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
     }
 
 }
